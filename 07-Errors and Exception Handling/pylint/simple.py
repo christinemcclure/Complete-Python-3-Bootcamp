004f7cb63b1948cf28e870e6497649ed4c1cfce0
Christine McClure <camcclure@gmail.com>
2022-11-01 16:37:10 -0500

    stopping for day

---
+++ 07-Errors and Exception Handling/pylint/simple.py
@@ -0,0 +1,16 @@
+'''
+Simple script
+'''
+
+
+def myfunc():
+    '''
+    simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+
+myfunc()
---
+++ 07-Errors and Exception Handling/unittests/cap.py
@@ -0,0 +1,11 @@
+
+'''
+simple script
+'''
+
+
+def cap_text(text):
+    '''
+    input string. output capitalized string
+    '''
+    return text.title()
